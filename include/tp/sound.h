08b34534aebe7683eab7d4cc1b64720522e9a322
Hartmut Seichter <hartmut@gmail.com>
2012-02-12 01:28:47 +0100

    some docs

--- include/tp/sound.h
+++ include/tp/sound.h
@@ -9,8 +9,6 @@
 
 TP_API void tpSoundTest(const char*);
 
-
-
 class TP_API tpSoundStream : public tpReferenced {
 public:
 
@@ -23,10 +21,28 @@ public:
 		kFormatStereo32
 	};
 
+	/**
+	  * Opens a sound file
+	  *
+	  * @param name filename/url of the file
+	  * @return true if successful
+	  */
 	virtual bool open(const tpString& name);
 
+	/**
+	  * read data from the stream into a buffer. The size of the
+	  * buffer needs to be preset and is adjusted on return.
+	  *
+	  * @param buffer storage where data is being read into
+	  * @param format returns the format of the audio data
+	  * @param freq returns the sample rate in Hz
+	  * @return true if successful
+	  */
 	virtual bool read(tpArray<tpChar> &buffer, tpUInt &format, tpUInt &freq);
 
+	/**
+	  * Closes the stream. Needs to be explicitly shut down
+	  */
 	virtual void close();
 
 private:
