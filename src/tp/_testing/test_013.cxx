3df78334c177cebf9e8916a0eefbd2b7b662d22e
Hartmut Seichter <hartmut@gmail.com>
2012-02-16 16:21:07 +0100

    added xml reader based on rapidxml + test program + Collada file it will be used later on

---
+++ src/tp/_testing/test_013.cxx
@@ -0,0 +1,38 @@
+
+
+#include <tp/xml.h>
+#include <tp/refptr.h>
+
+
+class MyCallback : public tpXMLNodeCallback {
+	void operator ()(const tpXML& xml)
+	{
+		for (tpXML::Path::const_iterator p = xml.getCurrentPath().begin();
+			 p != xml.getCurrentPath().end();
+			 p++)
+		{
+			printf("/%s",p->c_str());
+		}
+
+		printf("\n\tData '%s'\n",xml.getCurrentDataNode().c_str());
+
+		for (tpXML::Attributes::const_iterator a = xml.getCurrentAttributes().begin();
+			 a != xml.getCurrentAttributes().end();
+			 ++a)
+		{
+			printf("\t%s=%s\n",a->getKey().c_str(),a->getValue().c_str());
+		}
+	}
+};
+
+int main(int argc, char* argv[])
+{
+
+	tpRefPtr<tpXML> xml = new tpXML();
+
+	xml->setCallback(new MyCallback());
+
+	xml->parse("<we are=\"cool\"><some stuff=\"yes\">And also some text<tag a=\"b\"/></some><br/></we>");
+
+	return 0;
+}
---
+++ src/tp/xml.cxx
@@ -0,0 +1,74 @@
+#include <tp/xml.h>
+
+#define RAPIDXML_NO_EXCEPTIONS 1
+
+#include "rapidxml/rapidxml.hpp"
+
+#include <tp/log.h>
+
+#include <tp/refptr.h>
+
+void rapidxml::parse_error_handler(const char *what, void *where)
+{
+	tpLogError("tpXML error %s",what);
+}
+
+
+class tpXML;
+
+void rapidxml_dispatch(tpXML& xml,rapidxml::xml_node<>* node)
+{
+	//tpLogNotify("Node %s '%s'",node->name(),node->value());
+
+	xml.getCurrentDataNode() = tpString(node->value());
+	xml.getCurrentPath().push(tpString(node->name()));
+
+	xml.getCurrentAttributes().clear();
+
+	// push attributes
+	for (rapidxml::xml_attribute<>* attr = node->first_attribute();
+		 attr;
+		 attr = attr->next_attribute())
+	{
+		xml.getCurrentAttributes().add(tpString(attr->name()),tpString(attr->value()));
+		//tpLogNotify("\tAttribute %s = %s",attr->name(),attr->value());
+	}
+
+	xml.callback();
+
+	// iterate over child nodes
+	for (rapidxml::xml_node<>* child = node->first_node();
+		 child;
+		 child = child->next_sibling())
+	{
+		rapidxml_dispatch(xml,child);
+	}
+
+	xml.getCurrentPath().pop();
+}
+
+void
+tpXML::parse(const tpString& doc)
+{
+	// we need a copy
+	mDoc = doc;
+
+	using namespace rapidxml;
+
+	// now parse
+	xml_document<> xmldoc;
+	xmldoc.parse<parse_no_data_nodes>(mDoc.c_str());
+
+	rapidxml_dispatch(*this,xmldoc.first_node());
+
+	// sanity check
+	this->mAttributes.clear();
+	this->mData.empty();
+	this->mPath.empty();
+
+}
+
+void tpXML::callback() const
+{
+	if (mCallback.isValid()) (*mCallback)(*this);
+}
